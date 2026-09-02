5fe002a1a492d043be5d6d6c55fd75dad484f382
Bosik84 <85414343+Bosik84@users.noreply.github.com>
2026-09-02 13:53:41 +0300

    Update config.h

--- Inc/config.h
+++ Inc/config.h
@@ -161,7 +161,7 @@
 // Limitation settings
 #define I_MOT_MAX       10              // [A] Maximum single motor current limit
 #define I_DC_MAX        12              // [A] Maximum stage2 DC Link current limit for Commutation and Sinusoidal types (This is the final current protection. Above this value, current chopping is applied. To avoid this make sure that I_DC_MAX = I_MOT_MAX + 2A)
-#define N_MOT_MAX       200            // [rpm] Maximum motor speed limit
+#define N_MOT_MAX       800            // [rpm] Maximum motor speed limit
 
 // Field Weakening / Phase Advance
 #define FIELD_WEAK_ENA  0               // [-] Field Weakening / Phase Advance enable flag: 0 = Disabled (default), 1 = Enabled
@@ -526,7 +526,7 @@
   #define SPEED_COEFFICIENT       16384     // 1.0f
   #define STEER_COEFFICIENT       8192      // 0.5f Only active in Sideboard input
   // #define ADC_ALTERNATE_CONNECT             // use to swap ADC inputs
-   #define INVERT_R_DIRECTION                // Invert rotation of right motor
+  // #define INVERT_R_DIRECTION                // Invert rotation of right motor
   // #define INVERT_L_DIRECTION                // Invert rotation of left motor
   // #define DEBUG_SERIAL_USART3               // right sensor board cable, disable if I2C (nunchuk or lcd) is used!
 
